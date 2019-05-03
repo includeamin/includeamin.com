3deae28c4bfa59ff7be6c63d0d3ca398bf88c1c5
amin jamal <includeamin@MacBook-Pro.local>
2019-05-03 00:19:43 -0700

    main page has been edited

--- Menu/Projects/information/projects.py
+++ Menu/Projects/information/projects.py
@@ -1,1 +1,8 @@
-from flask import blueprints
+from flask import Blueprint
+
+Projects = Blueprint('WBC', __name__, template_folder='templates')
+
+
+@Projects.route('/projects/<name>')
+def get_projects_info(name):
+    return "{0}'s Info will ready ASAP".format(name)
