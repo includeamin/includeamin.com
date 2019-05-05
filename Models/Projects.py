20d181d28f1a056c8fa018cd3297788d3ffec9c9
amin jamal <includeamin@MacBook-Pro.local>
2019-05-05 09:38:29 -0700

    main page has been edited

---
+++ Models/Projects.py
@@ -0,0 +1,9 @@
+
+
+class Project:
+    def __init__(self,title,company,my_position,Description):
+        self.Title = title
+        self.Company = company
+        self.MyPosition = my_position
+        self.Description = Description
+
