77606288484777dc907c75867165beaaa34e9b6d
amin jamal <includeamin@MacBook-Pro.local>
2019-05-05 10:01:39 -0700

    main page has been edited

---
+++ Models/project_init.py
@@ -0,0 +1,21 @@
+data = [
+
+    {
+        "Title": "AnimUp",
+        "MyPosition": "Backend Engineer",
+        "Description": "This project is about media streaming.\n"
+                       "Animup is developed on microservice architecture\n"
+                       "this microservice consist of 7 services and these services are run on Docker ",
+        "Company": "Rastan",
+        "Website": "http://www.animup.ir/"
+    },
+    {
+        "Title": "WordsGame",
+        "MyPosition": "Backend Engineer",
+        "Description": "This project is about question race challenging,turn base multiplayer.\n"
+                       "WordsGame being developed on microservice architecture\n"
+                       "these microservice consist of 5 services and these services are running on Docker ",
+        "Company": "Rastan",
+        "Website": "None"},
+
+]
