c6303d9e0c918420da51bef8b41b35d0b182957f
amin jamal <includeamin@MacBook-Pro.local>
2019-08-03 22:08:58 +0430

    delete animation func [not compelete

--- Menu/ServiceStatus/ServiceStatus.py
+++ Menu/ServiceStatus/ServiceStatus.py
@@ -12,20 +12,20 @@ ServiceStatus = Blueprint('ServiceStatus', __name__, template_folder='templates'
 @ServiceStatus.route('/Services/Status/<name>')
 def get_service_status(name):
     try:
-        service_list = {"animup_micro": "http://89.32.251.131:3000/",
-                        "animup_auth": "http://89.32.251.131:3001/",
-                        'animup_user': 'http://89.32.251.131:3002/',
-                        'animup_accounting': 'http://89.32.251.131:3005/',
-                        'animup_animation': 'http://89.32.251.131:3003/',
-                        'animup_registery': 'http://89.32.251.131:5000/',
-                        'animup_upload': 'http://89.32.251.131:3004/',
+        service_list = {"animup_micro": "http://193.176.242.165:30030/",
+                        "animup_auth": "http://193.176.242.165:3001/",
+                        'animup_user': 'http://193.176.242.165:3002/',
+                        'animup_accounting': 'http://193.176.242.165:3005/',
+                        'animup_animation': 'http://193.176.242.165:3003/',
+                        'animup_registery': 'http://193.176.242.165:5000/',
+                        'animup_upload': 'http://193.176.243.180:3004/',
 
                         "words_micro": "http://89.32.251.131:3020/",
                         "word_auth": "http://89.32.251.131:3021/",
                         'word_user': 'http://89.32.251.131:3022/',
                         'words_accounting': 'http://89.32.251.131:3025/',
                         'word_game': 'http://89.32.251.131:3024/',
-                        'words_registery': 'http://89.32.251.131:5000/',
+                        'words_registery': 'http://193.176.242.165:5000/',
                         'words_resource': 'http://89.32.251.131:3023/',
 
                         'chichi_micro': 'http://chichiapp.ir:3000/',
