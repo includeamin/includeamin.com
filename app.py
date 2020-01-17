8d52c8d7625263e52d01b75d9ae49e58db33c9bf
includeamin <aminjamal10@gmail.com>
2020-01-17 15:16:36 +0330

    add terminal mode

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask,send_from_directory
+from flask import Flask, send_from_directory
 from Menu.WBC.app import WBC
 from Menu.Projects.information.projects import Projects
 from Menu.MainPage.MainPage import MainPage
 from Menu.ServiceStatus.ServiceStatus import ServiceStatus
 from Menu.Keyvan.KBooks import keyvan
 from flask_cors import CORS
+
 app = Flask(__name__)
 cors = CORS(app)
-
 
 app.register_blueprint(WBC)
 app.register_blueprint(Projects)
@@ -18,12 +18,13 @@ app.register_blueprint(keyvan)
 
 @app.route("/")
 def index():
-    return send_from_directory('./Static/',filename='index.html')
+    return send_from_directory('./Static/', filename='new_index.html')
 
 
-
-
+@app.route("/<file_name>")
+def get_files(file_name):
+    return send_from_directory('./Static/', filename=file_name)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=3000,debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
